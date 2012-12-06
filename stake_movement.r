b9cc0648ab2c3bb874bce9b943341ea84fa744ef
Sarah Supp <sarah@weecology.org>
2012-12-06 14:13:42 -0700

    moved code chunk for num indivs
    makes more sense near the occupancy stuff

--- stake_movement.r
+++ stake_movement.r
@@ -76,25 +76,7 @@ PB = subset(heteros, species == 'PB')
 PP = subset(heteros, species == 'PP')
 PF = subset(heteros, species == 'PF')
 
-##------- plot number of individuals captured in each period for each group, save in pdf file
-pdf(file="indivs_per_year.pdf",10,10)
-par(mfrow=c(1,1))
 
-plot_freq_by_prd(PB, "PB")
-plot_freq_by_prd(PP, "PP")
-plot_freq_by_prd(PF, "PF")
-plot_freq_by_prd(DM, "DM")
-plot_freq_by_prd(DO, "DO")
-plot_freq_by_prd(PE, "PE")
-plot_freq_by_prd(PM, "PM")
-plot_freq_by_prd(RM, "RM")
-plot_freq_by_prd(NAO, "NAO")
-plot_freq_by_prd(SF, "SF")
-plot_freq_by_prd(SH, "SH")
-plot_freq_by_prd(OT, "OT")
-plot_freq_by_prd(OL, "OL")
-
-dev.off()
 
 
 # DISTANCES, want to calculate distances moved for a single species
@@ -230,6 +212,26 @@ plot(pfyr, pfmo, xlim = c(0,1), ylim = c(0,1), xlab = "acros-year occupancy", yl
     textxy(rmyr, rmmo, "Cgran")
   abline(v = 0.5, lty = 2, col = 'gray40', lwd = 2)
   abline(h = 0.5, lty = 2, col = 'gray40', lwd = 2)
+
+##------- plot number of individuals captured in each period for each group, save in pdf file
+pdf(file="indivs_per_year.pdf",11,7.5)
+par(mfrow=c(3,1))
+
+plot_freq_by_prd(PB, "PB")
+plot_freq_by_prd(PP, "PP")
+plot_freq_by_prd(PF, "PF")
+plot_freq_by_prd(DM, "DM")
+plot_freq_by_prd(DO, "DO")
+plot_freq_by_prd(PE, "PE")
+plot_freq_by_prd(PM, "PM")
+plot_freq_by_prd(RM, "RM")
+plot_freq_by_prd(NAO, "NAO")
+plot_freq_by_prd(SF, "SF")
+plot_freq_by_prd(SH, "SH")
+plot_freq_by_prd(OT, "OT")
+plot_freq_by_prd(OL, "OL")
+
+dev.off()
 
 
 
