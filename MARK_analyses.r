19a938a30ab5cae13445625556ac13479fa4052b
sarahsupp <sara@weecology.org>
2013-01-21 13:01:11 -0700

    add dummy variables for state transistion
    also wrote a bunch of txt files to the datafiles folder

--- MARK_analyses.r
+++ MARK_analyses.r
@@ -12,9 +12,40 @@ MSdata <- convert.inp("do_mark.inp",
                       group.df=data.frame(sex=c("male","female","unidsex")))
 
 # Build up the model. Looking at sex effects on dispersal/survival
-MSprocess <- process.data(MSdata,model="Multistrata",begin.time=2000,
+MS.process <- process.data(MSdata,model="Multistrata",begin.time=2000,
                            group=c("sex"))
 
-MS.ddl <- make.design.data(MSprocess)
+MS.ddl <- make.design.data(MS.process)
+
+# Add dynamic dummy variable age class fields to the design data for Psi and p
+MS.ddl$Psi$hy=0
+MS.ddl$Psi$hy[MS.ddl$Psi$age==0&MS.ddl$Psi$stratum=="A"]=1
+MS.ddl$Psi$ahy=0
+MS.ddl$Psi$ahy[MS.ddl$Psi$Age>=1&MS.ddl$Psi$stratum=="A"]=1
+
+MS.ddl$p$hy=0
+MS.ddl$p$hy[MS.ddl$p$age==1&MS.ddl$p$stratum=="A"]=1
+MS.ddl$p$hy[MS.ddl$p$age==1&MS.ddl$p$stratum=="B"]=1
+MS.ddl$p$sy=0
+MS.ddl$p$sy[MS.ddl$p$age==2&MS.ddl$p$stratum=="A"]=1
+MS.ddl$p$asy=0
+MS.ddl$p$asy[MS.ddl$p$Age>=3&MS.ddl$p$stratum=="A"]=1
+MS.ddl$p$ahy=0
+MS.ddl$p$ahy[MS.ddl$p$Age>=2&MS.ddl$p$stratum=="B"]=1
+
+##### Add dummy variables for operating on specific states or transitions
+  # A = 1 (home), B = 2 (away)
+  # moving to B from anywhere, is risky
+  # moving to A from anywhere, is less risky (within home, "normal" movements)
+MS.ddl$Psi$toB=0
+MS.ddl$Psi$toB[MS.ddl$Psi$stratum=="1" & MS.ddl$Psi$tostratum=="2"]=1
+
+MS.ddl$Psi$toA=0
+MS.ddl$Psi$toA[MS.ddl$Psi$stratum=="2"]=1
+
+MS.ddl$p$strA=0
+MS.ddl$p$strA[MS.ddl$p$stratum=="A"]=1
+MS.ddl$p$strB=0
+MS.ddl$p$strB[MS.ddl$p$stratum=="B"]=1
 
 
