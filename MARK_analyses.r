4969545b2775596274c558fc5488bad12d481f61
sarahsupp <sara@weecology.org>
2013-01-21 10:25:19 -0700

    import the data

--- MARK_analyses.r
+++ MARK_analyses.r
@@ -1,4 +1,5 @@
-# for manipulating in MARK
+# for manipulating  rodent movement data in MARK
+## analyze survival and dispseral probabilities for species and guilds
 
 library(RMark) #can't install on mac? Update R version?
 
@@ -6,4 +7,7 @@ library(RMark) #can't install on mac? Update R version?
 wd = "/Users/sarah/Desktop/portal-rodent-dispersal/"
 setwd(wd)
 
+# bring in the inp file and convert it to RMark format (.txt needs to be .inp, change manually)
+do_mark_data <- convert.inp("do_mark.inp",
+                      group.df=data.frame(rage=c("male","female","unidsex")))
 
