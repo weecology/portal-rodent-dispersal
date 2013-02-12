429484ef3a111de66b2662800c67fd2380913672
Sarah Supp <sarah@weecology.org>
2013-02-12 12:24:01 -0700

    added code to find species-specific breakpoint
    should I use that instead in my Mark analysis? It's already pretty
    conservative for most species

--- stake_movement.r
+++ stake_movement.r
@@ -549,45 +549,67 @@ par(mfrow=c(3,2), mar=c(3,1.5,2,0.5), oma=c(1.5,2,1,1))
 v6 = seq(-3,500,6)
 docount = hist(dometers, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0, 350), 
                xlab = "meters", main = 'DO - krat')      
+xline(expm1(mean(log1p(dometers))+sd(log1p(dometers))), lwd = 2, col = "cadetblue")
 xline(Hgran_brkpt, lwd = 2, col = "indianred")
+
 dmcount = hist(dmmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0, 500), 
-               xlab = "meters", main = 'DM - krat')      
+               xlab = "meters", main = 'DM - krat')  
+xline(expm1(mean(log1p(dmmeters))+sd(log1p(dmmeters))), lwd = 2, col = "cadetblue")
 xline(Hgran_brkpt, lwd = 2, col = "indianred")
+
 pbcount = hist(pbmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0, 2000), 
-               xlab = "meters", main = 'PB - pocket mouse')      
+               xlab = "meters", main = 'PB - pocket mouse')   
+xline(expm1(mean(log1p(pbmeters))+sd(log1p(pbmeters))), lwd = 2, col = "cadetblue")
 xline(Hgran_brkpt, lwd = 2, col = "indianred")
+
 ppcount = hist(ppmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0, 600), 
                xlab = "meters", main = 'PP - pocket mouse')      
+xline(expm1(mean(log1p(ppmeters))+sd(log1p(ppmeters))), lwd = 2, col = "cadetblue")
 xline(Hgran_brkpt, lwd = 2, col = "indianred")
+
 pfcount = hist(pfmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0, 30), 
-               xlab = "meters", main = 'PF - pocket mouse')      
+               xlab = "meters", main = 'PF - pocket mouse')  
+xline(expm1(mean(log1p(pfmeters))+sd(log1p(pfmeters))), lwd = 2, col = "cadetblue")
 xline(Hgran_brkpt, lwd = 2, col = "indianred")
 
 pecount = hist(pemeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0,25), 
                xlab = "meters", main = 'PE - cactus mouse')
+xline(expm1(mean(log1p(pemeters))+sd(log1p(pemeters))), lwd = 2, col = "cadetblue")
 xline(Cgran_brkpt, lwd = 2, col = "indianred")
+
 pmcount = hist(pmmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0,15),
                xlab = "meters", main = 'PM - deer mouse')
+xline(expm1(mean(log1p(pmmeters))+sd(log1p(pmmeters))), lwd = 2, col = "cadetblue")
 xline(foli_brkpt, lwd = 2, col = "indianred")
+
 rmcount = hist(rmmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0,15),
                xlab = "meters", main = 'RM - harvest mouse')
+xline(expm1(mean(log1p(rmmeters))+sd(log1p(rmmeters))), lwd = 2, col = "cadetblue")
 xline(foli_brkpt, lwd = 2, col = "indianred")
 
 nacount = hist(naometers, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0,25), 
                xlab = "meters", main = 'NA - pack rat')
+xline(expm1(mean(log1p(naometers))+sd(log1p(naometers))), lwd = 2, col = "cadetblue")
 xline(nao_brkpt, lwd = 2, col = "indianred")
+
 shcount = hist(shmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0,15), 
                xlab = "meters", main = 'SH - cotton rat')
+xline(expm1(mean(log1p(shmeters))+sd(log1p(shmeters))), lwd = 2, col = "cadetblue")
 xline(nao_brkpt, lwd = 2, col = "indianred")
+
 sfcount = hist(sfmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0,15), 
                xlab = "meters", main = 'SF - cotton rat')
+xline(expm1(mean(log1p(sfmeters))+sd(log1p(sfmeters))), lwd = 2, col = "cadetblue")
 xline(nao_brkpt, lwd = 2, col = "indianred")
 
 otcount = hist(otmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0,100), 
                xlab = "meters", main = 'OT - grasshopper mouse')
+xline(expm1(mean(log1p(otmeters))+sd(log1p(otmeters))), lwd = 2, col = "cadetblue")
 xline(ins_brkpt, lwd = 2, col = "indianred")
+
 olcount = hist(olmeters, breaks = v6, col = 'gray60', xlim = c(0,500), ylim = c(0,15), 
                xlab = "meters", main = 'OL - grasshopper mouse')
+xline(expm1(mean(log1p(olmeters))+sd(log1p(olmeters))), lwd = 2, col = "cadetblue")
 xline(ins_brkpt, lwd = 2, col = "indianred")
 
 dev.off()
