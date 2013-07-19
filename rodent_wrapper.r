786c61e5204dc9818e44c58dea8157c0b5f31483
sarahsupp <sara@weecology.org>
2013-07-19 15:25:52 -0500

    adding code to loop over all species to get life-history traits

--- rodent_wrapper.r
+++ rodent_wrapper.r
@@ -26,6 +26,8 @@ all$sex = as.character(all$sex)
 
 #subset data for 10 year of analysis
 all2 = subset(all, yr > 1999)
+#subset data where species are known (e.g., no "unidentified rodents" or genus-only)
+all2 = subset(all2, species!="DX" & species!="UR" & species!="RX" & species!="SX" & species!="PX")
 
 # give untagged individuals a unique 7-number code
 all2 = id_unknowns(all2, 16)
@@ -37,22 +39,36 @@ all2 = subsetDat(all2)
 #          calculate life-history details - reproduction, temporal persistence
 #---------------------------------------------------------------------------------
 
-spp_list = c("DO", "DM", "DS", "PI", "PP", "PB", "PF",
-             "RM", "RF","RO", "PE", "PP", "PM", "PL", BA",
-             "NAO", "SH", "SF", "SO",
+spplist = c("DO", "DM", "DS", "PI", "PP", "PB", "PF",
+             "RM", "RF","RO", "PE", "PM", "PL", "BA",
+             "NAO", "SH", "SF",
              "OT", "OL")
 
-for (spp in 1:length(spp_list)){
+for (i in 1:length(spplist)){
+  #subset species data
+  spdata = subset(all2, species == spplist[i])
+  
+    # proportion of years they were seen in
+    propyrs = length(unique(spdata$yr))/10
+    # average number of months they were seen in during years in which they were present
+    avgmos = mean_win_yr_occ(spdata)
+    # mean abundance within all years 
+    avgabun = allyrs_abun(spdata)
+    #mean abundances within all years on control plots only
+    conabun = allyrs_abun(subset(spdata, plot %in% c(1,2,4,8,9,11,12,14,17,22)))
+  
+  #subset females for each species
+  spdataF = subset(spdata, sex == "F")
+  
+    #average proportion of reproductive females by month across all years
+    reprd = mean_mo_repro(spdataF) #vector with 12 items
+    # proportion of reproductive females by month and year
+    reprdyr = mo_repro(spdataF) #matrix with 120 x 5
+    # track the number of times individual females uniquely reproduce within years
+    indreprd = indiv_repro(spdataF) #matix with cols "tag", "year" and "num_reprod"
 
 }
 
-# average proportion of reproductive females by month across all years
-
-# proportion of reproductive females by month and year
-
-# track the number of times females uniquely reproduce within years
-
-# proportion of years they were seen in
 
 # average number of months they were seen in during years in which they were present
 
