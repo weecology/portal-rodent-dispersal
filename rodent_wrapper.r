2fe60d3ed51cdf0d83fe0e1dbe0eb11a2e0e62b6
sarahsupp <sara@weecology.org>
2013-07-19 16:10:07 -0500

    fixed rodent_wrapper to more efficiently loop through all species while using far fewer lines of code to do so.

--- rodent_wrapper.r
+++ rodent_wrapper.r
@@ -50,35 +50,82 @@ for (i in 1:length(spplist)){
   
     # proportion of years they were seen in
     propyrs = length(unique(spdata$yr))/10
+      assign(paste(spplist[i], 'propyrs', sep = ""), propyrs)
     # average number of months they were seen in during years in which they were present
     avgmos = mean_win_yr_occ(spdata)
+      assign(paste(spplist[i], 'avgmos', sep = ""), avgmos)
     # mean abundance within all years 
     avgabun = allyrs_abun(spdata)
+      assign(paste(spplist[i], 'avgabun', sep = ""), avgabun)
     #mean abundances within all years on control plots only
     conabun = allyrs_abun(subset(spdata, plot %in% c(1,2,4,8,9,11,12,14,17,22)))
+      assign(paste(spplist[i], 'conabun', sep = ""), conabun)
   
   #subset females for each species
   spdataF = subset(spdata, sex == "F")
   
     #average proportion of reproductive females by month across all years
     reprd = mean_mo_repro(spdataF) #vector with 12 items
+      assign(paste(spplist[i], 'propyrs', sep = ""), reprd)
     # proportion of reproductive females by month and year
     reprdyr = mo_repro(spdataF) #matrix with 120 x 5
+      assign(paste(spplist[i], 'propyrs', sep = ""), reprodyr)
     # track the number of times individual females uniquely reproduce within years
     indreprd = indiv_repro(spdataF) #matix with cols "tag", "year" and "num_reprod"
-
+      assign(paste(spplist[i], 'propyrs', sep = ""), indreprd)
 }
-
-
-# average number of months they were seen in during years in which they were present
-
-# mean abundance within all years 
-
-# mean abundances within all years on control plots only
 
 
 #---------------------------------------------------------------------------------
 #          calculate movement distances, multi-state capture histories
 #---------------------------------------------------------------------------------
 
-# get a vector unique tags, then get a vector of distances moved for all recaptured individuals, by SPECIES
+for (i in 1:length(spplist)){
+  #subset species data
+  spdata = subset(all2, species == spplist[i])
+  
+    # get a vector unique tags, then get a vector of distances moved for all recaptured individuals, by SPECIES
+    tags = unique(spdata$tag)
+      assign(paste(spplist[i], 'propyrs', sep = ""), tags)
+    meters = distance_moved(spdata, tags)
+     assign(paste(spplist[i], 'propyrs', sep = ""), meters)
+}
+
+# concatenate core heteromyid data - used to ask if these species behave differently from others
+corehet = c(dmmeters, dometers, pbmeters, ppmeters)
+
+  # find breakpoints to use in MARK data structure for future analyses
+  # data reasonably well fits a lognormal distribution (eyeball and J. Powell)
+  # breakpoint = mean(logdata) + sd(logdata) of all the distances traveled by recaptured individuals    
+  # using log1p, and back transforming using expm1 should solve the problem of lots of zeros 
+  corehet_brkpt = expm1(mean(log1p(corehet)) + sd(log1p(corehet)))
+
+# Get MARK capture histories
+#------------------------------
+periods = c(261:380)
+exclosures = c(5, 7, 10, 16, 23, 24)
+krat_excl = c(5, 7, 10, 16, 23, 24, 3, 6, 13, 15, 18, 19, 20, 21)
+
+for (i in 1:length(spplist)){
+  #subset species data
+  spdata = subset(all2, species == spplist[i])
+  
+  if (i == 1) {
+  MARK = noplacelikehome(spdata, periods, krat_excl, corehet_brkpt)
+  }
+  
+  else {
+  nextMARK = noplacelikehome(spdata, periods, krat_excl, corehet_brkpt)
+  MARK = rbind(MARK, nextMARK)
+  }
+}
+
+write.table(MARK, file = "mark_datafiles//all_mark.inp", row.names = F, col.names = F, quote = F)
+
+
+#---------------------------------------------------------------------------------
+#          plot results
+#---------------------------------------------------------------------------------
+
+
+  
