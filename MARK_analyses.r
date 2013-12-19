6382e30fa8ca9a07fb482ee2e795f172f6562e2e
sarahsupp <sarah@weecology.org>
2013-12-19 15:56:23 -0500

    fixed a bug in the code

--- MARK_analyses.r
+++ MARK_analyses.r
@@ -265,8 +265,8 @@ write.csv(Snull_pnull_Psinull$results$real, paste("ms_null_real_",ms_data[1,6],"
 
 
 cat("End Code. Look for your csv files.",sep="\n",file="outfile.txt",append=TRUE)
-cat(paste("file", file, "is done", sep = " "),sep="\n",file="outfile.txt", append = TRUE)
-
+print( paste("file", files[f] " is done.", sep = ""))
+  
 rm(list=ls()[!ls() %in% c("f", "files")])   # clears the computer's memory of everything except the file list
 
 }
