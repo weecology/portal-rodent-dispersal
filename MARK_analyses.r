4d3c7de49c4363e97920483c3ca3f0157cafc8b7
sarahsupp <sarah@weecology.org>
2014-01-09 13:02:30 -0500

    reformatting mark code to use only species as covariate in the analysis.

--- MARK_analyses.r
+++ MARK_analyses.r
@@ -30,10 +30,8 @@ for (f in 1:length(files)){
   
   require(RMark)
   
-# bring in the inp file and convert it to RMark format - This file includes all the data from all the species 
-# files are carn_mark.inp, foli_mark.inp and gran_mark.inp
-ms_data <- convert.inp(files[f], group.df = data.frame(sex=c("male", "female", "unidentified")),
-                      covariates = c("species"))
+# bring in the inp files and conver to tRMark format 
+ms_data <- convert.inp(files[f], ], covariates = c("species"))
 
 #convert to factor
 #ms_data$guild = as.factor(ms_data$guild)
